24c612dd38a72ab20644c873f94dce6f623c840f
Ingvild Kinn Ekroll <ingvild.k.ekroll@ntnu.no>
2014-07-22 15:26:44 +0200

    Replaced approximation for small angles with the general expression

--- +Doppler/vectorDoppler.m
+++ +Doppler/vectorDoppler.m
@@ -1,7 +1,7 @@
 function vectorEst = vectorDoppler(R1,p)
-%Gives you the x and z velocity components - for now assuming that all
-%images have the same receive direction, resulting in an effective (Doppler) angle
-%half the size of the tx angle
+%Gives you the x and z velocity components, p.angles are the steering
+%angles with respect to the transducer normal. Assuming receive direction
+%normal to transducer.
 
 sizeR1 = size( squeeze(R1) );
 
@@ -11,7 +11,8 @@ if sizeR1(2) ~= 1
     temp = reshape(temp, [size(temp,1)*size(temp,2),size(temp,3)]);
     fMat = (reshape(angle(R1),[size(R1,1)*size(R1,2),size(R1,3)])*p.PRF/(2*pi) ).';
 
-     aMat = [-sin(p.angles/2); cos(p.angles/2)];
+    %aMat = [-sin(p.angles/2); cos(p.angles/2)]; % Works as an approximation to the truth for small steering angles
+    aMat = [-sin(p.angles) (1+cos(p.angles))]./2; 
 
     pseudoInv = pinv(aMat).';
     for kk = 1:size(fMat,2)
@@ -23,10 +24,11 @@ if sizeR1(2) ~= 1
     vectorEst = reshape(temp,[size(vectorEst,1), size(vectorEst,2), size(vectorEst,3)]);
 
 else
-    vectorEst = single(zeros(size(R1,1),2));
     fMat = ( angle(R1)*p.PRF/(2*pi) ).';
 
-    aMat = [-sin((p.angles/2).'); cos((p.angles/2).')];
+    %aMat = [-sin((p.angles/2).'); cos((p.angles/2).')]; % Works for small steering angles
+     aMat = [-sin(p.angles).'; (1+cos(p.angles)).']./2;
+
     pseudoInv = pinv(aMat).';
     vectorEst = pseudoInv*fMat';
  
