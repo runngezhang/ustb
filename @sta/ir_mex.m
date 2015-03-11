f5adf665f049371bfb17dc638ed15e9dd1a78705
alfonsom <alfonso.r.molares@ntnu.no>
2015-03-11 15:34:42 +0100

    Renamed mex filed to avoid redundancy
    Former-commit-id: 1f78e84f128a3836122cf5b20f8fa4ef9ab566d7

--- @sta/ir_mex.m
+++ @sta/ir_mex.m
@@ -13,7 +13,7 @@ function sig = ir_mex(h,r)
 
     switch(h.format)
         case E.signal_format.RF
-            sig=mex.stair_mex(h.data, ...       % data
+            sig=mex.stair(h.data, ...       % data
                 r.scan.x.', r.scan.z.',...      % pixel positions (m)
                 [h.geom(:,1) h.geom(:,3)],...   % probe geometry [x, z] (m,m)
                 h.c0,...                        % speed of sound (m/s)
@@ -22,7 +22,7 @@ function sig = ir_mex(h,r)
                 h.sampling_frequency,...        % sampling frequency [Hz]
                 h.initial_time);                % initial time [s]
         case E.signal_format.IQ
-            sig=mex.stair_mex(h.data, ...       % data
+            sig=mex.stair(h.data, ...       % data
                 r.scan.x.', r.scan.z.',...      % pixel positions (m)
                 [h.geom(:,1) h.geom(:,3)],...   % probe geometry [x, z] (m,m)
                 h.c0,...                        % speed of sound (m/s)
