482a84c6ef3089f92db7a6bf5096105cd1f02350
alfonsom <alfonso.r.molares@ntnu.no>
2015-11-25 10:49:09 +0100

    Modified low resolution mexfile interaction
    Former-commit-id: c1085958e7b2ea8405ac26f7db46e2955d42aeaa

--- @cpw/low_res.m
+++ @cpw/low_res.m
@@ -7,9 +7,10 @@ function [sig] = low_res(h,r)
 %       beamformed_signal   Matrix containig the beamformed raw data 
 %
 %   See also RECONSTRUCTION
-
-%   authors: Alfonso Rodriguez-Molares (alfonsom@ntnu.no)
-%   $Date: 2015/03/06 $
+%
+% date:     06.03.2015
+% updated:  01.10.2015
+% authors:  Alfonso Rodriguez-Molares <alfonso.r.molares@ntnu.no>
 
 
     % initial time -> performance test
@@ -20,24 +21,16 @@ function [sig] = low_res(h,r)
     assert(length(r.orientation)==1,'Calculation of low resolution images only works for single orientation. To be clarified.')
 
     % precompute transmit and receive apodization
-    xT=r.scan.x*ones(1,h.firings)-r.scan.z*tan(h.angle.'); % position of equivalent receive element -> Alfonso's equation 
-    h.transmit_apodization = r.calculate_apodization(r.orientation.transmit_beam,xT);
-    xR=ones(r.scan.pixels,1)*(h.geom(:,1).');                  % position of receive element
-    h.receive_apodization = r.calculate_apodization(r.orientation.receive_beam,xR);
+    xT=r.scan.x*ones(1,h.firings)-r.scan.z*tan(h.angle.');          % position of equivalent receive element -> Alfonso's equation 
+    zT=r.scan.x*zeros(1,h.firings);                            % assuming planar transducer
+    h.transmit_apodization = r.calculate_apodization(r.orientation.transmit_beam,xT,zT);
+    xR=ones(r.scan.pixels,1)*(h.geom(:,1).');                       % position of receive element
+    zR=ones(r.scan.pixels,1)*(h.geom(:,3).');                  % position of transmit and receive element    
+    h.receive_apodization = r.calculate_apodization(r.orientation.receive_beam,xR,zR);
 
     switch(h.format)
         case E.signal_format.RF
-            sig=mex.cpwlr(h.data, ...       % data
-                r.scan.x.', r.scan.z.',...      % pixel positions (m)
-                [h.geom(:,1) h.geom(:,3)],...   % probe geometry [x, z] (m,m)
-                h.c0,...                        % speed of sound (m/s)
-                h.angle.',...                   % angles of the plane waves (rad)
-                h.transmit_apodization,...      % transmit aperture [pixels, channels]
-                h.receive_apodization,...       % receive aperture [pixels, channels]
-                h.sampling_frequency,...        % sampling frequency [Hz]
-                h.initial_time);                % initial time [s]
-        case E.signal_format.IQ
-            sig=mex.cpwlr(h.data, ...       % data
+            sig=mex.cpwlr(h.data, ...           % data
                 r.scan.x.', r.scan.z.',...      % pixel positions (m)
                 [h.geom(:,1) h.geom(:,3)],...   % probe geometry [x, z] (m,m)
                 h.c0,...                        % speed of sound (m/s)
@@ -46,7 +39,20 @@ function [sig] = low_res(h,r)
                 h.receive_apodization,...       % receive aperture [pixels, channels]
                 h.sampling_frequency,...        % sampling frequency [Hz]
                 h.initial_time,...              % initial time [s]
-                h.modulation_frequency);        % modulation frequency [Hz]
+                0,...                           % modulation frequency [Hz]
+                1);                             % verbose mode
+        case E.signal_format.IQ
+            sig=mex.cpwlr(h.data, ...           % data
+                r.scan.x.', r.scan.z.',...      % pixel positions (m)
+                [h.geom(:,1) h.geom(:,3)],...   % probe geometry [x, z] (m,m)
+                h.c0,...                        % speed of sound (m/s)
+                h.angle.',...                   % angles of the plane waves (rad)
+                h.transmit_apodization,...      % transmit aperture [pixels, channels]
+                h.receive_apodization,...       % receive aperture [pixels, channels]
+                h.sampling_frequency,...        % sampling frequency [Hz]
+                h.initial_time,...              % initial time [s]
+                h.modulation_frequency,...      % modulation frequency [Hz]
+                1);                             % verbose mode
         otherwise
             error('Unknown signal format!');
     end
