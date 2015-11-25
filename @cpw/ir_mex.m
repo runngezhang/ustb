95e2512f807a1355a2d84edc38f4706c21fdab7e
alfonsom <alfonso.r.molares@ntnu.no>
2015-11-25 10:45:25 +0100

    Cheap workaround for singleton dimension problem. I have to come back to this by changing all mex files
    Former-commit-id: 93a8a43f261126aedc046215f6b3057f2ee36752

--- @cpw/ir_mex.m
+++ @cpw/ir_mex.m
@@ -11,31 +11,43 @@ function [sig] = ir_mex(h,r)
 %   authors: Alfonso Rodriguez-Molares (alfonsom@ntnu.no)
 %   $Date: 2015/01/28 $
                 
+    % cheap workaround for matlab singular dimension problem
+    unidimensional=false;
+    if ndims(h.data)==2
+        unidimensional=true;
+        h.data=repmat(h.data,[1 1 1 2]);
+    end
+    
     switch(h.format)
         case E.signal_format.RF
-            sig=mex.cpwir(h.data, ...       % data
+            sig=mex.cpwir(h.data, ...           % data
                 r.scan.x.', r.scan.z.',...      % pixel positions (m)
                 [h.geom(:,1) h.geom(:,3)],...   % probe geometry [x, z] (m,m)
-                h.c0,...                        % speed of sound (m/s)
-                h.angle.',...                   % angles of the plane waves (rad)
-                h.transmit_apodization,...      % transmit aperture [pixels, channels]
-                h.receive_apodization,...       % receive aperture [pixels, channels]
-                h.sampling_frequency,...        % sampling frequency [Hz]
-                h.initial_time);                % initial time [s]
-        case E.signal_format.IQ
-            sig=mex.cpwir(h.data, ...       % data
-                r.scan.x.', r.scan.z.',...      % pixel positions (m)
-                [h.geom(:,1) h.geom(:,3)],...   % probe geometry [x, z] (m,m)
-                h.c0,...                        % speed of sound (m/s)
+                double(h.c0),...                % speed of sound (m/s)
                 h.angle.',...                   % angles of the plane waves (rad)
                 h.transmit_apodization,...      % transmit aperture [pixels, channels]
                 h.receive_apodization,...       % receive aperture [pixels, channels]
                 h.sampling_frequency,...        % sampling frequency [Hz]
                 h.initial_time,...              % initial time [s]
-                h.modulation_frequency);        % modulation frequency [Hz]
+                0);
+        case E.signal_format.IQ
+            sig=mex.cpwir(double(h.data), ...       % data
+                double(r.scan.x.'), double(r.scan.z.'),...      % pixel positions (m)
+                double([h.geom(:,1) h.geom(:,3)]),...   % probe geometry [x, z] (m,m)
+                double(h.c0),...                % speed of sound (m/s)
+                double(h.angle).',...                   % angles of the plane waves (rad)
+                double(h.transmit_apodization),...      % transmit aperture [pixels, channels]
+                double(h.receive_apodization),...       % receive aperture [pixels, channels]
+                double(h.sampling_frequency),...        % sampling frequency [Hz]
+                double(h.initial_time),...              % initial time [s]
+                double(h.modulation_frequency));        % modulation frequency [Hz]
         otherwise
             error('Unknown signal format!');
     end
     
+    % cheap workaround for matlab singular dimension problem
+    if unidimensional
+        sig=sig(:,1);
+    end
 end
 
