25a85e53637d2ff241f2fbce92e0dc035c6eb1e6
Andreas Østvik <Andreas stvik>
2016-10-26 14:34:43 +0200

    Improved logic of function. Enabled reconstruction argument from either pyus og ustb.
    Former-commit-id: 738898f2c8bd7508d57f0cbcc131f27dea9ea454

--- +pyus/image_reconstruction.m
+++ +pyus/image_reconstruction.m
@@ -1,19 +1,28 @@
-function elapsed_time = image_reconstruction(file, recons, implem, method)
+function [elapsed_time, res_data] = image_reconstruction(file, recons, implem, method)
 data_path = which(file);
 
 ustbdata = py.pyus.Interop.USTBInterop.USTBInteropFilter();
 ustbdata.param.update(pyargs('path',data_path));
+ustbdata.update();
 
-pyusrecons = pyus.create_pyusrecons(recons);
+if isequal(class(recons), class(py.pyus.Interop.USTBInterop.Reconstruction))
+    pyusrecons = recons;
+else
+    pyusrecons = pyus.create_pyusrecons(recons);
+end
 
 tic;
 bf = py.pyus.Filters.Beamforming.BeamformingFilters.BeamformingFilter();
 bf.set_inputs(ustbdata);
 bf.param.update(pyargs('recons', pyusrecons, 'method', method));
 
-recons.data = pyus.fromNdarray(bf.data);
-elapsed_time=toc;
+res_data = pyus.from_ndarray(bf.data);
 
-recons.format = E.signal_format.(char(ustbdata.attr{'format'}));
+if ~isequal(class(recons), class(py.pyus.Interop.USTBInterop.Reconstruction))
+    recons.data = res_data';
+    recons.format = E.signal_format.(char(ustbdata.attr{'format'}));
+end
+elapsed_time=toc;
+    
 end
 
