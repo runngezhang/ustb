e51cfbd580bbf87ffe91c6999511fd17e00d7988
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2017-03-09 11:13:00 +0100

    We made the analytical signal great again, by finding bug that Hilbert can only be called on 2d matrices.
    Former-commit-id: f940aabd3e12769f3f77005c06c3f64ddb86ffb9

--- @us_dataset/make_analytical_signal.m
+++ @us_dataset/make_analytical_signal.m
@@ -1,10 +1,23 @@
 function make_analytical_signal(h)
-%MAKE_ANALYTICAL_SIGNAL Summary of this function goes here
-%   Detailed explanation goes here
+%MAKE_ANALYTICAL_SIGNAL Creates the analytical signal by taking the Hilbert
+%   transform
+%
+%   authors : Ole Marius Hoel Rindal <olemarius@olemarius.net>
+%             Stine Myren Hverven <stinemhv@ifi.uio.no>
+%   $Date   : 2017/03/06 
+%             2017/03/09 : Fixed bug for matrices lager than 2D. However
+%                          this made the code very slow.
 
 assert(h.format==E.signal_format.RF,'Signal format is not RF. Analytical signal can only be made from RF data.');
 
-h.data = hilbert(h.data);
+wb = waitbar(size(h.data,4),'Taking Hilbert Transform')
+for f=1:size(h.data,4)
+    waitbar(f/size(h.data,4))
+    for m=1:size(h.data,3)
+        h.data(:,:,m,f) = hilbert(h.data(:,:,m,f));
+    end
+end
+close(wb)
 h.format = E.signal_format.AS;
 end
 
