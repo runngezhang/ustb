ae4a4c99de202b91822fe476d0964f0518c2eee5
alfonsom <alfonso.r.molares@ntnu.no>
2017-03-15 17:45:47 +0100

    Set up matrix_array example. Something not working on the calculation of the apodization, I believe.
    Former-commit-id: d45a30efaae4abeef9eedef9a74afbe7462c33de

--- +huff/matrix_array.m
+++ +huff/matrix_array.m
@@ -1,4 +1,4 @@
-classdef matrix_array < probe 
+classdef matrix_array < huff.probe 
 %MATRIX_ARRAY   2D matrix array definition. Child of PROBE class
 %
 %   See also PROBE, BEAM, PHANTOM
@@ -22,11 +22,11 @@ classdef matrix_array < probe
             if ~isempty(h.pitch_x)&&~isempty(h.pitch_y)&&~isempty(h.N_x)&&~isempty(h.N_y) 
                 
                 if isempty(h.element_width)
-                    h.element_width=h.pitch;
+                    h.element_width=h.pitch_x;
                 end
                 
                 if isempty(h.element_height)
-                    h.element_height=h.element_width;
+                    h.element_height=h.pitch_y;
                 end
                 
                 % compute element center location
@@ -36,7 +36,7 @@ classdef matrix_array < probe
                 y0=y0-mean(y0);
                 
                 % meshgrid
-                [X Y]=meshgrid(x,y);
+                [X Y]=meshgrid(x0,y0);
 
                 % assign geometry
                 h.geometry=[X(:) Y(:) zeros(h.N_x*h.N_y,3) h.element_width*ones(h.N_x*h.N_y,1) h.element_height*ones(h.N_x*h.N_y,1)]; % probe geometry
@@ -49,32 +49,32 @@ classdef matrix_array < probe
         function h=set.pitch_x(h,in_pitch)
             assert(numel(in_pitch)==1, 'The input should be a scalar in [m]');
             h.pitch_x=in_pitch;
-            h.update();
+            h=h.update();
         end
         function h=set.pitch_y(h,in_pitch)
             assert(numel(in_pitch)==1, 'The input should be a scalar in [m]');
             h.pitch_y=in_pitch;
-            h.update();
+            h=h.update();
         end
         function h=set.N_x(h,in_N_elements)
             assert(numel(in_N_elements)==1, 'The input should be a scalar');
-            h.N_elements_x=in_N_elements;
-            h.update();
+            h.N_x=in_N_elements;
+            h=h.update();
         end
         function h=set.N_y(h,in_N_elements)
             assert(numel(in_N_elements)==1, 'The input should be a scalar');
-            h.N_elements_y=in_N_elements;
-            h.update();
+            h.N_y=in_N_elements;
+            h=h.update();
         end
         function h=set.element_width(h,in_width)
             assert(numel(in_width)==1, 'The input should be a scalar in [m]');
             h.element_width=in_width;
-            h.update();
+            h=h.update();
         end
         function h=set.element_height(h,in_height)
             assert(numel(in_height)==1, 'The input should be a scalar in [m]');
             h.element_height=in_height;
-            h.update();
+            h=h.update();
         end
     end
     
---
+++ examples/simulator/matrix_array/STA_matrix_array.m
@@ -0,0 +1,80 @@
+% Example of Synthetic Transmit Aperture simulation with USTB built-in Fresnel simulator
+
+%   authors: Alfonso Rodriguez-Molares (alfonso.r.molares@ntnu.no)
+%   $Date: 2017/03/11$
+
+clear all;
+close all;
+
+%% PHANTOM
+pha=huff.phantom();
+pha.sound_speed=1540;            % speed of sound [m/s]
+pha.points=[0,  0, 20e-3, 1];    % point scatterer position [m]
+fig_handle=pha.plot();             
+             
+%% PROBE
+prb=huff.matrix_array();
+prb.N_x=16;                 % number of elements 
+prb.N_y=16;                 % number of elements 
+prb.pitch_x=600e-6;         % probe pitch in azimuth [m]
+prb.pitch_y=600e-6;         % probe pitch in azimuth [m]
+prb.element_width=570e-6;   % element width [m]
+prb.element_height=570e-6;  % element height [m]
+prb.plot(fig_handle);
+
+%% PULSE
+pul=huff.pulse();
+pul.center_frequency=3e6;       % transducer frequency [MHz]
+pul.fractional_bandwidth=0.6;     % fractional bandwidth [unitless]
+pul.plot([],'2-way pulse');
+
+%% SEQUENCE GENERATION
+seq=huff.wave();
+for n=1:prb.N_elements 
+    seq(n)=huff.wave();
+    seq(n).probe=prb;
+    seq(n).source.xyz=[prb.x(n) prb.y(n) prb.z(n)];
+    
+    seq(n).apodization.window=huff.window.sta;
+    seq(n).apodization.apex=seq(n).source;
+    
+    seq(n).sound_speed=pha.sound_speed;
+    
+    % show source
+    fig_handle=seq(n).source.plot(fig_handle);
+end
+
+%% SIMULATOR
+sim=simulator();
+
+% setting input data 
+sim.phantom=pha;                % phantom
+sim.pulse=pul;                  % transmitted pulse
+sim.probe=prb;                  % probe
+sim.sequence=seq;               % beam sequence
+sim.sampling_frequency=41.6e6;  % sampling frequency [Hz]
+
+% we launch the simulation
+channel_data=sim.go();
+ 
+%% SCAN
+sca=huff.linear_scan(linspace(-4e-3,4e-3,200).', linspace(18e-3,22e-3,100).');
+sca.plot(fig_handle,'Scenario');    % show mesh
+ 
+%% BEAMFORMER
+bmf=beamformer();
+bmf.channel_data=channel_data;
+bmf.scan=sca;
+bmf.receive_apodization.window=huff.window.tukey50;
+bmf.receive_apodization.f_number=1.7;
+bmf.receive_apodization.apex.distance=Inf;
+bmf.transmit_apodization.window=huff.window.tukey50;
+bmf.transmit_apodization.f_number=1.7;
+bmf.transmit_apodization.apex.distance=Inf;
+
+% beamforming
+b_data=bmf.go(@bmf.matlab,@postprocess.coherent_compound);
+
+% show
+b_data.plot();
+
