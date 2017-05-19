f4942e20d0b939d1475acbcdfa3222a506463a1d
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2017-05-18 20:06:18 -0400

    Added pulse to the CPWC channel data in the Verasonics class.
    Former-commit-id: b3aada0b6b48f6b90ee498f1c3d95871fb64edb4

--- @verasonics/verasonics.m
+++ @verasonics/verasonics.m
@@ -79,6 +79,10 @@ classdef verasonics < handle
             end
             channel_data.sequence = seq;
             
+            
+            %% Save Pulse
+            channel_data.pulse = uff.pulse(double(h.Trans.frequency*10^6));
+            
             %% Convert channel data from Verasonics format to USTB format
             no_samples = h.Receive(1).endSample;
             data = zeros(no_samples, h.Resource.Parameters.numRcvChannels, length(seq), h.Resource.RcvBuffer(1).numFrames);
