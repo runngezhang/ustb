d49329d335089730584247afd0db84fc56095fe3
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2017-05-26 14:17:41 -0400

    Added example and CPWC datasets recorded on the Alpinion scanner.
    Former-commit-id: 2db7f2739f567004def4b5af03618fc82b31ee76

---
+++ examples/uff/CPWC_UFF_Alpinion.m
@@ -0,0 +1,101 @@
+%% Reading data from an UFF file recorded from a Alpinion scanner
+%
+% In this example we show how to read channel and beamformed data from a
+% UFF (Ultrasound File Format) file recorded with an Alpinion scanner.
+% You will need an internet connection to download data.
+%
+% _by Ole Marius Hoel Rindal <olemarius@olemarius.net>
+%  and Muyinatu Lediju Bell <mledijubell@jhu.edu> 26.05.2017_
+
+%% Checking the file is in the path
+%
+% To read data from a UFF file the first we need is, you guessed it, a UFF
+% file. We check if it is on the current path and download it from the USTB
+% websever.
+
+% data location
+url='http://ustb.no/datasets/';      % if not found data will be downloaded from here
+local_path = [ustb_path(),'/data/']; % location of example data in this computer
+addpath(local_path);
+
+% We have to different Alpinion CPWC datasets, comment out the one to use
+filename='Alpinion_L3-8_CPWC_hyperechoic_scatterers.uff';
+%filename='Alpinion_L3-8_CPWC_hypoechoic.uff';
+
+% check if the file is available in the local path & downloads otherwise
+tools.download(filename, url, local_path);
+
+%% Reading channel data
+%
+% Now that the file is on the path let us create a *UFF* object to interact
+% with the file. We open it in "append" mode.
+
+uff_file=uff(filename)
+
+%%
+%
+% Let's first check if we are lucky and the file allready contains
+% beamformed_data that we can display.
+display=true;
+content = uff_file.index('/',display);
+
+has_b_data = false;
+for i = 1:length(content)
+    if strcmp(content{i}.class,'uff.beamformed_data')
+        has_b_data = true; % We found a beamformed data object!
+    end
+end
+
+
+%%
+%
+%
+% If it doesn't have any beamformed data at least it should have some
+% channel_data. So let's read that.
+% If the file did have beamformed data, will will jump straight to the
+% display part later on :)
+if ~has_b_data
+    
+    % Read the channel data
+    channel_data=uff_file.read('/channel_data');
+    
+    %%
+    %
+    % And then the normal routine of defining the scan,
+    
+    sca=uff.linear_scan();
+    sca.x_axis = linspace(channel_data.probe.x(1),channel_data.probe.x(end),512).';
+    sca.z_axis = linspace(1e-3,50e-3,512).';
+    
+    %%
+    %
+    % setting up and running the beamforming
+    bmf=beamformer();
+    bmf.channel_data=channel_data;
+    bmf.scan=sca;
+    
+    bmf.receive_apodization.window=uff.window.tukey25;
+    bmf.receive_apodization.f_number=1.7;
+    bmf.receive_apodization.apex.distance=Inf;
+    
+    bmf.transmit_apodization.window=uff.window.tukey25;
+    bmf.transmit_apodization.f_number=1.7;
+    bmf.transmit_apodization.apex.distance=Inf;
+    
+    b_data=bmf.go({process.das_mex process.coherent_compounding});
+    %%
+    %
+    % Now we can save this beamformed image to that file, so that we don't have
+    % to wait for the beamforming again.
+    uff_file.write(b_data,'b_data');
+end
+
+%%
+%
+% And finally display the image.
+b_data.plot();
+
+%%
+%
+% Let's look at the info given about this dataset
+channel_data.print_info()
