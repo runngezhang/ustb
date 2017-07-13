a3e6a45f9e6a28e2cd1a03a03318b25480b13652
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2017-07-13 11:20:53 +0200

    Added code and examples on how to read data recorded with an Alpinion research scanner to the USTB.
    Former-commit-id: 81e32805903ed3bd9c8f343be2eeb377d45ea0a4

---
+++ examples/alpinion/FI/L3-8_FI.m
@@ -0,0 +1,61 @@
+% Read FI linear dataset from the Alpinion Research scanner
+%
+%   This examples shows how to load FI data recorded on the Alpinion
+%   Research scanner with L3-8_OMHR_Focused.py example sequence the into a 
+%   channel_data object and beamform it with the USTB routines. The data 
+%   should be in the path /data/Alpinion/FI_linear/ from the root folder of
+%   the USTB. The data can be downloaded from: 
+%   https://www.dropbox.com/sh/wv7sa88dvhfszcl/AABYgOJixkKvQM_Q3jJlvFVWa?dl=0
+%
+%   date : 11.07.2017
+%   author: Ole Marius Hoel Rindal (olemarius@olemarius.net)
+
+clear all; close all
+
+local_path = [ustb_path(),'/data/']; % location of example data in this computer
+
+tag = 'FI_hyperechoic_scatterers';  % Dataset 1
+%tag = 'FI_hypoechoic';             % Dataset 2
+data_folder  = [local_path,'/Alpinion/FI_linear/',tag];
+
+%% initiate Alpinion object pointing to files with data
+alp = alpinion();
+alp.data_folder = data_folder;
+
+%% Create channel_data object
+number_of_frames = 4;
+start_frame = 2;
+channel_data = alp.read_FI_linear(number_of_frames,start_frame);
+%% Define image scan
+z_axis=linspace(1e-3,55e-3,512).';
+sca=uff.linear_scan();
+idx = 1;
+for n=1:numel(channel_data.sequence)
+    sca(n)=uff.linear_scan(channel_data.sequence(n).source.x,z_axis);
+end
+%% Define BEAMFORMER
+bmf=beamformer();
+bmf.channel_data=channel_data;
+bmf.scan=sca;
+bmf.receive_apodization.window=uff.window.tukey25;
+bmf.receive_apodization.f_number=1.7;
+bmf.receive_apodization.apex.distance=Inf;
+
+% Do beamforming
+b_data=bmf.go({process.das_mex process.stack});
+%% Show image
+b_data.plot(11,['Alpinion FI dataset'],60);
+%%
+if strfind(tag, 'scatterers')
+    channel_data.name = {'FI dataset of hyperechoic cyst and points scatterers recorded on an Alpinion scanner with a L3-8 Probe from a CIRS General Purpose Ultrasound Phantom'};
+else
+    channel_data.name = {'FI dataset of hypoechic cyst recorded on an Alpinion scanner with a L3-8 Probe from a CIRC General Purpose Ultrasound Phantom'};
+end
+channel_data.author = {'Ole Marius Hoel Rindal <olemarius@olemarius.net>','Muyinatu Lediju Bell <mledijubell@jhu.edu>'}; 
+channel_data.reference = {'www.ultrasoundtoolbox.com'};
+channel_data.version = {'1.0.2'};
+
+%%
+uff_filename = ['Alpinion_L3-8_',tag,'.uff']
+uff_file=uff(uff_filename);
+uff_file.write(channel_data,'channel_data');
