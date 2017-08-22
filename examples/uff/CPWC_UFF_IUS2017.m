657ce8dd15e085307c535b66a3ad048d890c00e2
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2017-08-22 23:27:47 +0200

    Updated minimal example
    Former-commit-id: 5650c8ef618cffc9b0f9c741a30f7157c388be6c

--- examples/uff/CPWC_UFF_IUS2017.m
+++ examples/uff/CPWC_UFF_IUS2017.m
@@ -1,16 +1,17 @@
-%% Read channel data from a CPWC dataset from UFF file
+% Read CPWC channel data from a UFF file
 channel_data=uff.read_object('./CPWC_PSF.uff','/channel_data');
 
-%% Define scan (the pixels in the image)
+% Define scan (the pixel locations in the image)
 scan=uff.linear_scan();
 scan.x_axis = linspace(-2e-3,2e-3,200).';
 scan.z_axis = linspace(19e-3,21e-3,100).';
 
-%% Set up beamformer for CPWC
+% Set up beamformer for CPWC
 bmf=beamformer();
 bmf.channel_data=channel_data;
 bmf.scan=scan;
 
+% Set up apodization, use boxcar with f-number 1.2 on tx and rx
 bmf.receive_apodization.window=uff.window.boxcar;
 bmf.receive_apodization.f_number=1.2;
 bmf.receive_apodization.origo.distance=Inf;
