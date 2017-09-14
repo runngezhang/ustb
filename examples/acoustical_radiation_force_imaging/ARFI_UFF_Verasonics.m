f2fbee154bcb5e15a067f22e9f0bf9014303f47a
Svetoslav Nikolov <sin@bkmed.dk>
2017-09-14 09:35:34 +0200

    Renamed ustb_data_path to data_path for shortness
    Renamed ustb_data_path to data_path. Modified the script ARFI_UFF_Verasonics.m which uses the function.
    
    
    Former-commit-id: b864a61055adcc9f96d889edaddcdbbf2b133bf6

--- examples/acoustical_radiation_force_imaging/ARFI_UFF_Verasonics.m
+++ examples/acoustical_radiation_force_imaging/ARFI_UFF_Verasonics.m
@@ -13,7 +13,7 @@ clear all; close all;
 
 % data location
 url='http://ustb.no/datasets/';            % if not found data will be downloaded from here
-local_path = [ustb_data_path(), filesep];  % location of example data on this computer
+local_path = [data_path(), filesep];  % location of example data on this computer
 
 filename='ARFI_dataset.uff';
 
