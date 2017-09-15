9ea81529626f15c36a2cde8a31e0d2827c49bb3d
alfonsom <alfonso.r.molares@ntnu.no>
2017-09-15 11:13:24 +0200

    Changed name convention in data_path() and solved a little bug with the warning message.
    Former-commit-id: b0f3558d0aea8847d96427d9a0dbbba85359a38a

--- data_path.m
+++ data_path.m
@@ -1,18 +1,24 @@
-function ustb_data = ustb_data_path()
-%USTB_PATH Returns the path (root) of data for USTB.
-% Returns the value of environment variable 'USTB_DATA' if it exists,
+function path_out = data_path()
+%DATA_PATH Returns the path (root) of data for USTB.
+% Returns the value of environment variable 'DATA_PATH' if it exists,
 % otherwise it returns path to `data` folder in USTB
 %
 %   Usage: out = ustb_data_path()
 %
-    ustb_data = getenv('USTB_DATA'); 
+    path_out = getenv('DATA_PATH'); 
     
-    if isempty(ustb_data),
-        [ustb_path, ~, ~] = fileparts(which('ustb_data_path'));
-        ustb_data = [ustb_path, filesep, 'data'];
+    if isempty(path_out),
+        path_out = [ustb_path(), filesep, 'data'];
     end
     
-    if ~exist(ustb_data, 'dir'),
-        warning('USTB:DataPathNotExist', ['"', ustb_data, '"', ' does not exist!']);
+    if ~exist(path_out, 'dir'),
+        % we must duplicate filesep to get the message through warning
+        ind_sep=find(path_out==filesep);
+        for n=1:numel(ind_sep)
+            path_out = [path_out(1:ind_sep(n)) path_out(ind_sep(n):end)];
+            if(n<numel(ind_sep)) ind_sep(n+1:end)=ind_sep(n+1:end)+1; end
+        end
+        
+        warning('USTB:DataPathNotExist', ['"', path_out, '"', ' does not exist!']);
     end
 end
