a6c242b4d974f4174a3bf4e531b7844cd51a5993
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2017-10-10 11:59:38 +0200

    Rewrote to fit new das implementation.

--- examples/fresnel/phased_array/FI_phased_array_multiframe.m
+++ examples/fresnel/phased_array/FI_phased_array_multiframe.m
@@ -135,36 +135,28 @@ channel_data=sim.go();
 % generate a sector scan. *scan* too has a useful *plot* method it can call.
 
 depth_axis=linspace(5e-3,80e-3,256).';
-sca=uff.sector_scan();
-for n=1:N_beams
-    sca(n)=uff.sector_scan('azimuth_axis',azimuth_axis(n),'depth_axis',depth_axis);
-    sca(n).plot(fig_handle,'Scenario');    
-end
+scan=uff.sector_scan('azimuth_axis',azimuth_axis,'depth_axis',depth_axis);
  
-%% Beamformer
+ 
+%% Midprocess
 %
 % With *channel_data* and a *scan* we have all we need to produce an
-% ultrasound image. We now use a USTB structure *beamformer*, that takes an
-% *apodization* structure in addition to the *channel_data* and *scan*.
+% ultrasound image. We now use a USTB structure *midprocess*, that takes an
+% *apodization* structure in addition to the *channel_data* and *scan*, and 
+% returns a *beamformed_data*.
 
-pipe=pipeline();
-pipe.channel_data=channel_data;
-pipe.scan=sca;
+mid=midprocess.das();
+mid.channel_data=channel_data;
+mid.scan=scan;
+mid.dimension = dimension.both();
 
-pipe.receive_apodization.window=uff.window.tukey50;
-pipe.receive_apodization.f_number=1.7;
+mid.transmit_apodization.window = uff.window.scanline;
 
-%% 
-%
-% The *beamformer* structure allows you to implement different beamformers 
-% by combination of multiple built-in *processes*. By changing the *process*
-% chain other beamforming sequences can be implemented. It returns yet 
-% another *UFF* structure: *beamformed_data*.
+mid.receive_apodization.window=uff.window.tukey50;
+mid.receive_apodization.f_number=1.7;
 
-% To achieve the goal of this example, we use delay-and-sum (implemented in 
-% the *das_matlab()* process) and then stack each line into a single image.
 
-b_data=pipe.go({midprocess.das_mex() postprocess.stack()});
+b_data=mid.go();
 
 %% Plotting the data
 %
