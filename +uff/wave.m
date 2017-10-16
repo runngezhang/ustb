030a1bd028803a4676604b7259a46abbb048991d
alfonsom <alfonso.r.molares@ntnu.no>
2017-10-16 10:55:09 +0200

    Changed the name of t0_compensation for delay. Changed the name of the dependant parameters delay for delay_values. Included a waveform enumeration that allows to define other kind of sources (such as photoacoustic) and define plane waves in a more intuitive way.

--- +uff/wave.m
+++ +uff/wave.m
@@ -1,91 +1,131 @@
 classdef wave < uff
-%wave   Wave definition
-%
-%   See also WAVE, SOURCE, PHANTOM, PROBE, PULSE
-
-%   authors: Alfonso Rodriguez-Molares (alfonso.r.molares@ntnu.no)
-%   $Date: 2017/02/22 $
-
-    %% public properties
+    %WAVE   UFF class that describes a transmitted wave 
+    %   WAVE contains information to describe a wave: plannar, spherical,
+    %   or photoacoustic, and the apodization used to produce it.
+    %
+    %   WAVE.WAVEFRONT defines the type of wave produced: uff.wavefront.plane, 
+    %   uff.wavefront.plane, uff.wavefront.spherical, or uff.wavefront.photoacoustic.  
+    %
+    %   WAVE.SOURCE defines the wave attitude. If WAVE.WAVEFRONT is uff.wavefront.spherical 
+    %   then WAVE.SOURCE defines the point in space from which the wave originated. 
+    %   If WAVE.SOURCE is behind the plane z=0 then the spherical wave will be
+    %   diverging. If WAVE.SOURCE is in front of the plane z=0 the the spherical 
+    %   wave will be converging. If the WAVE.TYPWAVEFRONTE is uff.wavefront.plane then 
+    %   WAVE.SOURCE defines the orientation through the azimuth and elevation 
+    %   angles, i.e. WAVE.SOURCE.DISTANCE becomes meaningless. If the WAVE.WAVEFRONT is
+    %   uff.wavefront.photoacoustic then WAVE.SOURCE is ignored.
+    %
+    %   WAVE.APODIZATION is a UFF.APODIZATION class used to compute the apodization
+    %   values that generate the UFF.WAVE.
+    %
+    %   WAVE.INITIAL_TIME defines the time interval between the reference
+    %   time t0 and the start of acquisition for this particular wave. We 
+    %   refer to reference time, or time zero, as the moment the wave passes 
+    %   through the origin of coordinates (0,0,0).
+    %
+    %   Compulsory properties:
+    %    wavefront          % WAVEFRONT enumeration class
+    %    source             % POINT class
+    %    apodization        % APODIZATION class
+    %
+    %   Optional properties:
+    %    probe              % PROBE class 
+    %    event              % index of the transmit/receive events this wave refers to
+    %    sound_speed        % reference speed of sound
+    %    initial_time       % time interval between generation and acquistion 
+    %
+    %   Example:
+    %         wave = uff.wave();
+    %
+    %   See also UFF.POINT, UFF.BEAMFORMED_DATA, UFF.PROBE
+    
+    %   authors: Alfonso Rodriguez-Molares <alfonso.r.molares@ntnu.no>
+    %            Ole Marius Hoel Rindal <olemarius@olemarius.net>
+    %
+    %   $Last updated: 2017/10/13$
+    
+    %% compulsory properties
     properties  (Access = public)
-        probe            % PROBE class
-        apodization      % APODIZATION class
-        source           % POINT class
-        event            % index of the transmit/receive events this wave refers to
-        sound_speed      % reference speed of sound
-        t0_compensation=0% dinstance to compensate for t0 in m
-                         % The USTB assumes that time zero corresponds to 
-                         % the moment the transmitted wave passes through 
-                         % the origin of coordinates (0; 0; 0).
+        wavefront         % WAVEFRONT enumeration class
+        source            % POINT class
+        apodization       % APODIZATION class
     end
     
+    %% optional properties
+    properties  (Access = public)
+        probe              % PROBE class 
+        event              % index of the transmit/receive event this wave refers to
+        delay              % time interval between t0 and acquistion start
+        sound_speed        % reference speed of sound
+    end
+    
+    
     %% dependent properties
-    properties  (Dependent)   
-        N_elements         % number of elements 
-        delay              % delay [s]        
+    properties  (Dependent)
+        N_elements         % number of elements
+        delay_values       % delay [s]
         apodization_values % apodization [unitless]
     end
     
-    %% constructor
+    %% constructor -> uff constructor
     methods (Access = public)
-        function h=wave()
-            %WAVE   Constructor of WAVE class
-            %
-            %   Syntax:
-            %   h = wave()
-            %
-            %   See also WAVE, SOURCE, PHANTOM, PROBE, PULSE
+        function h=wave(varargin)
+            h = h@uff(varargin{:});
             
-            h.source=uff.point();
+            % default values
+            h.source        = uff.point();
+            h.wavefront     = uff.wavefront.spherical;
+            h.sound_speed   = 1540; 
+            h.delay         = 0;    
         end
     end
     
     %% plot methods
     methods
         function fig_handle=plot(h,figure_handle_in)
-          
-           if nargin>1 && not(isempty(figure_handle_in))
-                fig_handle=figure(figure_handle_in); 
+            
+            if nargin>1 && not(isempty(figure_handle_in))
+                fig_handle=figure(figure_handle_in);
             else
                 fig_handle=figure();
             end
-
-          % probe geometry
-          x = [(h.probe.x-h.probe.width/2.*cos(h.probe.theta)).'; (h.probe.x+h.probe.width/2.*cos(h.probe.theta)).'; (h.probe.x+h.probe.width/2.*cos(h.probe.theta)).'; (h.probe.x-h.probe.width/2.*cos(h.probe.theta)).'];
-          y = [(h.probe.y-h.probe.height/2.*cos(h.probe.phi)).'; (h.probe.y-h.probe.height/2.*cos(h.probe.phi)).'; (h.probe.y+h.probe.height/2.*cos(h.probe.phi)).'; (h.probe.y+h.probe.height/2.*cos(h.probe.phi)).'; ];
-          z = zeros(size(x));
-          c = linspace(0,1,h.probe.N_elements);
-          
-          subplot(1,2,1);
-          % draw flatten elements
-          %fill3(x*1e3,y*1e3,z*1e3,c); grid on; axis equal tight; hold on;
-          % draw delays
-          %plot3(h.probe.x*1e3,h.probe.y*1e3,h.delay*1e6,'r.'); grid on; axis tight;
-          plot(h.probe.x*1e3,h.delay*1e6,'r.'); grid on; axis tight; 
-          xlabel('x [mm]');
-          %ylabel('y [mm]');
-          ylabel('delay [\mus]');
-          ylim([min([min(h.delay*1e6) -1e-3]) max([max(h.delay*1e6) 1e-3])]);
-          set(gca,'fontsize',14);
-          title('Delays');
-          
-          subplot(1,2,2);
-          % draw flatten elements
-          %fill3(x*1e3,y*1e3,z*1e3,c); grid on; axis equal tight; hold on;
-          % draw apodization
-          %plot3(h.probe.x*1e3,h.probe.y*1e3,h.apodization_values,'r.'); grid on; axis tight;
-          plot(h.probe.x*1e3,h.apodization_values,'r.'); grid on; axis tight;
-          xlabel('x [mm]');
-          %ylabel('y [mm]');
-          ylabel('Apodization');
-          set(gca,'fontsize',14);
-          title('Apodization');
-          
+            
+            % probe geometry
+            x = [(h.probe.x-h.probe.width/2.*cos(h.probe.theta)).'; (h.probe.x+h.probe.width/2.*cos(h.probe.theta)).'; (h.probe.x+h.probe.width/2.*cos(h.probe.theta)).'; (h.probe.x-h.probe.width/2.*cos(h.probe.theta)).'];
+            y = [(h.probe.y-h.probe.height/2.*cos(h.probe.phi)).'; (h.probe.y-h.probe.height/2.*cos(h.probe.phi)).'; (h.probe.y+h.probe.height/2.*cos(h.probe.phi)).'; (h.probe.y+h.probe.height/2.*cos(h.probe.phi)).'; ];
+            z = zeros(size(x));
+            c = linspace(0,1,h.probe.N_elements);
+            
+            subplot(1,2,1);
+            % draw flatten elements
+            %fill3(x*1e3,y*1e3,z*1e3,c); grid on; axis equal tight; hold on;
+            % draw delays
+            %plot3(h.probe.x*1e3,h.probe.y*1e3,h.delay*1e6,'r.'); grid on; axis tight;
+            plot(h.probe.x*1e3,h.delay_values*1e6,'r.'); grid on; axis tight;
+            xlabel('x [mm]');
+            %ylabel('y [mm]');
+            ylabel('delay [\mus]');
+            ylim([min([min(h.delay*1e6) -1e-3]) max([max(h.delay*1e6) 1e-3])]);
+            set(gca,'fontsize',14);
+            title('Delays');
+            
+            subplot(1,2,2);
+            % draw flatten elements
+            %fill3(x*1e3,y*1e3,z*1e3,c); grid on; axis equal tight; hold on;
+            % draw apodization
+            %plot3(h.probe.x*1e3,h.probe.y*1e3,h.apodization_values,'r.'); grid on; axis tight;
+            plot(h.probe.x*1e3,h.apodization_values,'r.'); grid on; axis tight;
+            xlabel('x [mm]');
+            %ylabel('y [mm]');
+            ylabel('Apodization');
+            set(gca,'fontsize',14);
+            title('Apodization');
+            
         end
     end
     
     %% set methods
-    methods  
+    methods
         function h=set.apodization(h,in_apodization)
             assert(isa(in_apodization,'uff.apodization'), 'The apodization is not an APODIZATION class. Check HELP APODIZATOR');
             h.apodization=in_apodization;
@@ -98,15 +138,20 @@ classdef wave < uff
             assert(isa(in_probe,'uff.probe'), 'The probe is not a PROBE class. Check HELP PROBE');
             h.probe=in_probe;
         end
+        function h=set.wavefront(h,in_wavefront)
+            assert(isa(in_wavefront,'uff.wavefront'), 'The probe is not a WAVEFRONT class. Check HELP WAVEFRONT');
+            h.wavefront=in_wavefront;
+            if (h.wavefront == uff.wavefront.plane) h.source.distance = Inf; end
+        end
     end
     
     %% get methods
-    methods  
+    methods
         function value=get.N_elements(h)
             value=h.probe.N_elements;
         end
         
-        function value=get.delay(h)
+        function value=get.delay_values(h)
             assert(~isempty(h.probe),'The PROBE must be inserted for delay calculation');
             assert(~isempty(h.sound_speed),'The sound speed must be inserted for delay calculation');
             
@@ -123,7 +168,7 @@ classdef wave < uff
         end
         
         function value=get.apodization_values(h)
-            if isempty(h.apodization) 
+            if isempty(h.apodization)
                 value=ones(1,h.probe.N_elements);
             else
                 h.apodization.probe=h.probe; % set values not yet in apodization
