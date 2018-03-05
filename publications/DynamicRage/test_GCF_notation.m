31c7f585f2366330c97598eed0074bb400337d35
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2018-03-05 16:25:14 +0100

    Testing some more.

--- publications/DynamicRage/test_GCF_notation.m
+++ publications/DynamicRage/test_GCF_notation.m
@@ -5,19 +5,27 @@ s = [1 2 3 4 5 4 3 2 1];
 % NB, only for the "low frequency" region -M0:M0
 M = length(s);
 idx = 1;
-M0 = 2;
-p = zeros(1,length(-M0:M0));
+M0 = 5;
+P = zeros(1,length(-M0:M0));
+S = zeros(1,length(-M0:M0));
+tic
 for a = -M0:M0
     for m = 1:M
-       p(idx) =  p(idx) + exp(-j*pi*(a))*s(m)*exp(-j*2*pi*(m-1)*(a)/M);
+       P(idx) =  P(idx) + exp(-j*pi*(a))*s(m)*exp(-j*2*pi*(m-1)*(a)/M);
+       S(idx) =  S(idx) + s(m)*exp(-j*2*pi*(m-1-M/2)*(a)/M);
     end
     idx = idx + 1
 end
+toc
 
 figure(1);clf
-plot(-M0:M0,abs(p));
+plot(-M0:M0,abs(P));hold on;
+plot(-M0:M0,abs(S),'b*');
 title(['The low frequency region M0 = ',num2str(M0)]);
 
+tic
+fft(s);
+toc
 
 %Plot the FFT and the shifted FFT
 figure(2);clf
