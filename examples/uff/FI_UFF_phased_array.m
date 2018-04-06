8c5cbeda9b9bebe558dcb9e4b536a61f9ecf219a
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2018-04-06 14:01:57 +0200

    Updated phased array example and added beamforming with MLAs

--- examples/uff/FI_UFF_phased_array.m
+++ examples/uff/FI_UFF_phased_array.m
@@ -1,4 +1,24 @@
+%% Example script demonstrating phased array focused imaging (FI) with a
+% dataset recorded from the Verasonics Vantage 256 with a P4-2v probe.
+%
+% The dataset was used in the publication;
+% Rindal, O. M. H., Aakhus, S., Holm, S., &  
+%		 Austeng, A. (2017). Hypothesis of Improved  
+%		 Visualization of Microstructures in the  
+%		 Interventricular Septum with Ultrasound and  
+%		 Adaptive Beamforming. Ultrasound in Medicine and  
+%		 Biology, 43(10), 2494?2499.  
+%		 https://doi.org/10.1016/j.ultrasmedbio.2017.05.023 
+%
+% If you want to use this dataset you have to reference the article.
+%
+
+
+
+% Clear up
 clear all;close all;
+
+% Read the data, poentitally download it
 url='http://ustb.no/datasets/';      % if not found downloaded from here
 local_path = [ustb_path(),'/data/']; % location of example data
 addpath(local_path);
@@ -8,17 +28,17 @@ filename='Verasonics_P2-4_parasternal_long.uff';
 % check if the file is available in the local path or downloads otherwise
 tools.download(filename, url, local_path);
 channel_data = uff.read_object([local_path, filename],'/channel_data');
-scan = uff.read_object([local_path, filename],'/scan');
 
 %%
 % Print info about the dataset. Remeber that if you want to use this dataset
 % you have to reference this article!
 channel_data.print_authorship
 
-%% Do beamforming
-
+%% Throw away most of the dataset to make the beamformign run faster. 
+% Comment out this line if you want to run it on all 50 frames
 channel_data.N_frames = 1;
 
+%% Do beamforming
 depth_axis=linspace(0e-3,110e-3,512).';
 azimuth_axis=zeros(channel_data.N_waves,1);
 for n=1:channel_data.N_waves
@@ -28,17 +48,36 @@ end
 scan=uff.sector_scan('azimuth_axis',azimuth_axis,'depth_axis',depth_axis);
 
 mid=midprocess.das();
-mid.dimension = dimension.both();
 mid.channel_data=channel_data;
+mid.dimension = dimension.both();
 mid.scan=scan;
-
 mid.transmit_apodization.window=uff.window.scanline;
 mid.receive_apodization.window=uff.window.tukey25;
 mid.receive_apodization.f_number = 1.7;
-% This will result in a beamformed_data object with the delayed and not
-% summed channel data.
+
 b_data = mid.go();
 
-%% Plot the two images
+%% Plot image
 b_data.plot(3,['DAS']);
-mid.receive_apodization.plot([],32);
+
+
+%% Beamform the image with 4 MLA's per scan line with two overlapping
+MLA = 4;
+scan_MLA=uff.sector_scan('azimuth_axis',...
+    linspace(channel_data.sequence(1).source.azimuth,channel_data.sequence(end).source.azimuth,...
+    length(channel_data.sequence)*MLA)'...
+    ,'depth_axis',depth_axis);
+
+mid_MLA=midprocess.das();
+mid_MLA.channel_data=channel_data;
+mid_MLA.dimension = dimension.both();
+mid_MLA.scan=scan_MLA;
+mid_MLA.transmit_apodization.window=uff.window.scanline;
+mid_MLA.transmit_apodization.MLA = MLA;
+mid_MLA.transmit_apodization.MLA_overlap = MLA/2;
+mid_MLA.receive_apodization.window=uff.window.tukey25;
+mid_MLA.receive_apodization.f_number = 1.7;
+
+b_data_MLA = mid_MLA.go();
+%% Plot the image 
+b_data_MLA.plot(4,['DAS with MLAs']);
