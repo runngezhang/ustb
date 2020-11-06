48bc7eb31e890136ca9c7dc40e71671061740121
Stefano Fiorentini <stefano.fiorentini@ntnu.no>
2020-11-06 12:07:14 +0100

    reverted changes to tools.low_pass() and tools.band_pass()

--- +tools/band_pass.m
+++ +tools/band_pass.m
@@ -1,19 +1,24 @@
-function [pf, h, w] = band_pass(p, Fs, F)
-    % function pf = band_pass(p, Fs, [upper_freq_on upper_freq_off])
+function [filtered_p,h,w] = band_pass(p, Fs, F, dev)
+    % function filtered_p = band_pass(p, Fs, F, dev)
     
-    % Filter specification
-    A = [0, 1, 0];                                          % band type: 0='stop', 1='pass'
-    dev = [1e-3, 1e-3, 1e-3];                            	% max ripple in pass-band and stop-band
-    [N, Wn, beta, ftype] = kaiserord(F, A, dev, Fs);        % window parameters
-    b = fir1(N, Wn, ftype, kaiser(N+1,beta), 'noscale');    % filter design
+    if ~exist('dev')
+        dev=[1e-3 1e-3 1e-3];     % ripple/attenuation spec
+    end
     
-    [h, w] = freqz(b);
+    % filter specification
+    A=[0 1 0];                % band type: 0='stop', 1='pass'
+    [M,Wn,beta,typ]= kaiserord(F,A,dev,Fs);  % window parameters
+    b=fir1(M,Wn,typ,kaiser(M+1,beta),'noscale'); % filter design
+    
+    [h,w] = freqz(b);
 
-    % Filtering
-    [~, Ns] = max(abs(hilbert(b)));
-    pf = filter(b, 1, p, [], 1);  
-    
-    % Remove invalid samples
-    pf(1:Ns, :, :, :) = [];
+    % filtering
+    sz=size(p);
+    filt_delay=round((length(b)-1)/2);
+    filtered_p=filter(b,1,[p; zeros([filt_delay sz(2:end)])],[],1);
+
+    % correcting the delay
+    filtered_p=filtered_p((filt_delay+1):end,:,:,:);
+
 end
 
--- +tools/low_pass.m
+++ +tools/low_pass.m
@@ -1,21 +1,20 @@
-function [pf, h, w] = low_pass(p, Fs, F)
-    % function pf = low_pass(p, Fs, [upper_freq_on, upper_freq_off])
+function [filtered_p,h,w] = low_pass(p,Fs,F)
+    % function filtered_p = low_pass(p,Fs,[upper_freq_on upper_freq_off])
     
-    % Filter specification
-    A = [1, 0];                                             % band type: 0='stop', 1='pass'
-    dev = [1e-2, 1e-3];                                     % max ripple in pass-band and stop-band
-    [N, Wn, beta, ftype] = kaiserord(F, A, dev, Fs);        % window parameters
-    b = fir1(N, Wn, ftype, kaiser(N+1,beta), 'noscale');    % filter design
+    % filter specification
+    A=[1 0];                % band type: 0='stop', 1='pass'
+    dev=[1e-3 1e-3];        % ripple/attenuation spec
+    [M,Wn,beta,typ]= kaiserord(F,A,dev,Fs);  % window parameters
+    b=fir1(M,Wn,typ,kaiser(M+1,beta),'noscale'); % filter design
 
-    [h, w] = freqz(b);
+    [h,w] = freqz(b);
     
-    % Filtering
-    [~, Ns] = max(abs(hilbert(b)));
-    Sp = size(p);
+    % filtering
+    filt_delay=round((length(b)-1)/2);
+    filtered_p=filter(b,1,[p; zeros(filt_delay,size(p,2),size(p,3),size(p,4))],[],1);
+
+    % correcting the delay
+    filtered_p=filtered_p((filt_delay+1):end,:,:);
     
-    pf = filter(b, 1, p, [], 1);
-    
-    % Remove invalid samples
-    pf(1:Ns, :, :, :) = [];
 end
 
