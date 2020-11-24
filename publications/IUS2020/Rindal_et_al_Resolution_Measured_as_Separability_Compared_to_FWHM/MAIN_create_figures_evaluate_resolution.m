7a2cd793a4a62f7f760d93d2613b950b7168e5c8
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2020-11-24 13:22:40 +0100

    Code cleanup for code used in publication Rindal, O. M. H., Rodriguez-Molares, A., & Austeng, A. (2020). Resolution Measured as Separability Compared to Full Width Half Maximum for Adaptive Beamformers. IEEE International Ultrasonics Symposium, IUS, 1, 16–19.

--- publications/IUS2020/Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM/MAIN_create_figures_evaluate_resolution.m
+++ publications/IUS2020/Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM/MAIN_create_figures_evaluate_resolution.m
@@ -1,3 +1,24 @@
+% Example generating the plots in Resolution Measured as Separability Compared to Full Width Half Maximum for Adaptive Beamformers
+% If using some of this data or code you should cite: 
+% Rindal, O. M. H., Rodriguez-Molares, A., & Austeng, A. (2020). Resolution
+% Measured as Separability Compared to Full Width Half Maximum for Adaptive 
+% Beamformers. IEEE International Ultrasonics Symposium, IUS, 1, 16–19.
+% 
+% Abstract: Spatial resolution is defined as a system’s ability to separate 
+% targets using some kind of criterion, such as the Rayleigh criterion. However, 
+% in practice, lateral resolution is often evaluated by measuring the Full 
+% Width Half Maximum (FWHM at -6dB) of the point spread function (PSF).
+% We hypothesize that FWHM overestimates the system resolution for some
+% adaptive beamformers, compared to using the Rayleigh criterion. 
+% Simulation results seem to confirm this hypothesis.
+%
+% Author: Ole Marius Hoel Rindal <olemarius@olemarius.net>
+% Date 24.11.2020
+
+%% Run script to create beampatterns
+create_beampatterns_plot
+
+%% Load the data
 clear all;
 close all;
 filename = [ustb_path(),'/data/FieldII_CPWC_point_scatterers_res_v2.uff'];
@@ -19,22 +40,19 @@ b_data_ebmv.read([filename],'/b_data_ebmv');
 b_data_dmas.read([filename],'/b_data_dmas');
 
 sca = b_data_das.scan;
-%%
-
+% Separating distances used in the simulation. See 
 separating_distance = [4 2 1 0.5 0.44 0.4];
-%%
 
-b_data_das.plot([],'DAS')
-b_data_cf.plot([],'CF')
-b_data_pcf.plot([],'PCF')
-b_data_gcf.plot([],'GCF')
-b_data_mv.plot([],'MV')
-b_data_ebmv.plot([],'EBMV')
-b_data_dmas.plot([],'F-DMAS')
+%% Plot the images
+b_data_das.plot([],'DAS');
+b_data_cf.plot([],'CF');
+b_data_pcf.plot([],'PCF');
+b_data_gcf.plot([],'GCF');
+b_data_mv.plot([],'MV');
+b_data_ebmv.plot([],'EBMV');
+b_data_dmas.plot([],'F-DMAS');
 
-%%
-frames = [1 2 3 4 5 6 7];
-
+% Get images in separate variables
 img{1} = b_data_das.get_image();
 img{2} = b_data_cf.get_image();
 img{3} = b_data_pcf.get_image();
@@ -51,20 +69,47 @@ tags{5} = 'MV';
 tags{6} = 'EBMV';
 tags{7} = 'F-DMAS';
 
-[FileName,path] = uiputfile('movie.mp4','Save movie loop as');
-vidObj = VideoWriter([path,filesep,FileName],'MPEG-4');
+%% Plot resolution measured as FWHM
+img_none{1} = b_data_das.get_image('none');
+img_none{2} = b_data_cf.get_image('none');
+img_none{3} = b_data_pcf.get_image('none');
+img_none{4} = b_data_gcf.get_image('none');
+img_none{5} = b_data_mv.get_image('none');
+img_none{6} = b_data_ebmv.get_image('none');
+img_none{7} = b_data_dmas.get_image('none');
+
+f = figure(88);clf;
+clear res;
+for m = 1:7
+    format long
+    lateral =  img{m}(103,:,1);
+    
+    [res(m)] = calculate_6dB_resolution(sca.x_axis*10^3,lateral,1,88,m);
+    
+    ylim(gca,[-30 0])
+    xlim(gca,[-1 1])
+    set(gca,'FontSize',15);
+end
+
+subplot(2,7,[8:14]);
+bar(1:7,res)
+xlim([0.5 7.5])
+ylabel('FWHM [mm]');
+xticks(1:7)
+xticklabels(tags)
+set(gca,'FontSize',13);
+set(gcf,'Position',[10 163 1454 464])
+saveas(f,[ustb_path,filesep,'publications',filesep,'IUS2020',filesep,'Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM',filesep,'figures',filesep,'FWHM_res_v2'],'eps2c')  
+saveas(f,[ustb_path,filesep,'publications',filesep,'IUS2020',filesep,'Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM',filesep,'figures',filesep,'FWHM_res_v2'],'png')  
+
+%% Create plot and movie of images with displacement indicated
+frames = [1 2 3 4 5 6 7];
+
+FileName = 'movie_res.mp4';
+vidObj = VideoWriter([FileName],'MPEG-4');
 vidObj.Quality = 100;
 vidObj.FrameRate = 1;
 open(vidObj);
-%for i = 1:size(h.all_images,3)
-%    
-%    set(h.image_handle,'CData',h.all_images(:,:,i));
-%    title([h.in_title,', Frame = ',num2str(i),'/',num2str(size(h.all_images,3))]);
-%    drawnow();
-%    writeVideo(vidObj, getframe(h.figure_handle));
-    
-%end
-%close(vidObj)
 
 idx = 1;
 for i = frames
@@ -112,73 +157,31 @@ end
 close(vidObj)
 
 
-%%
-
-figure
-imagesc(img{end-2}(:,:,end))
-
-%%
+%% Measure separability 
 clear separability;
 frames = [2:7];
 for i = frames
-    figure(101+i);clf;hold all;
-for m= 1:7
-    
+    %figure(101+i);clf;hold all;
+    for m= 1:7
         normalized_lateral{m} =  img{m}(105,:,i)-max(img{m}(105,:,i));
         
-        
-        [dummy,idx_start] = min(abs(sca.x_axis*1000-(-2.5)))
-        [dummy,idx_stop] = min(abs(sca.x_axis*1000-(1)))
-        [dummy,idx_zero] = min(abs(sca.x_axis*1000))
+        [dummy,idx_start] = min(abs(sca.x_axis*1000-(-2.5)));
+        [dummy,idx_stop] = min(abs(sca.x_axis*1000-(1)));
+        [dummy,idx_zero] = min(abs(sca.x_axis*1000));
         min_value = normalized_lateral{m}(idx_zero);
         [max_value,idx_max] = max(normalized_lateral{m}(idx_start:idx_stop));
         
-        plot(sca.x_axis(idx_start:idx_stop)*1000,normalized_lateral{m}(idx_start:idx_stop));
-        plot(sca.x_axis(idx_start+idx_max-1)*1000,max_value,'*');
-        plot(sca.x_axis(idx_zero)*1000,min_value,'*');
+        %plot(sca.x_axis(idx_start:idx_stop)*1000,normalized_lateral{m}(idx_start:idx_stop));
+        %plot(sca.x_axis(idx_start+idx_max-1)*1000,max_value,'*');
+        %plot(sca.x_axis(idx_zero)*1000,min_value,'*');
         
         separability(m,i-1) = max_value-min_value;
     end
 end
 
-%%
-img_none{1} = b_data_das.get_image('none');
-img_none{2} = b_data_cf.get_image('none');
-img_none{3} = b_data_pcf.get_image('none');
-img_none{4} = b_data_gcf.get_image('none');
-img_none{5} = b_data_mv.get_image('none');
-img_none{6} = b_data_ebmv.get_image('none');
-img_none{7} = b_data_dmas.get_image('none');
 
-f = figure(88);clf;
-clear res;
-for m = 1:7
-    format long
-    m
-    lateral =  img{m}(103,:,1);
-    
-    [res(m)] = Compute_6dB_Resolution_edit(sca.x_axis*10^3,lateral,1,88,m);
-    
-    ylim(gca,[-30 0])
-    xlim(gca,[-1 1])
-    set(gca,'FontSize',15);
-    %figure(89);hold all;
-    %plot(db(abs(lateral./max(lateral))))
-end
-
-subplot(2,7,[8:14]);
-bar(1:7,res)
-xlim([0.5 7.5])
-ylabel('FWHM [mm]');
-xticks(1:7)
-xticklabels(tags)
-set(gca,'FontSize',13);
-set(gcf,'Position',[10 163 1454 464])
-saveas(f,[ustb_path,filesep,'publications',filesep,'IUS2020',filesep,'Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM',filesep,'figures',filesep,'FWHM_res_v2'],'eps2c')  
-saveas(f,[ustb_path,filesep,'publications',filesep,'IUS2020',filesep,'Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM',filesep,'figures',filesep,'FWHM_res_v2'],'png')  
-
-%%
-separability_lim = 6;
+%% Create summary plots of separability
+separability_lim = 6; 
 
 f = figure;
 subplot(211);hold all;
@@ -199,7 +202,7 @@ ylabel('Separability [dB]');
 set(gca,'FontSize',12)
 saveas(f,[ustb_path,filesep,'publications',filesep,'IUS2020',filesep,'Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM',filesep,'figures',filesep,'separability_1'],'eps2c')  
 
-f = figure
+f = figure;
 subplot(212);hold all;
 plot((separability(1,:)>separability_lim)-0.15,'-*','LineWidth',2,'DisplayName',tags{1})
 plot((separability(2,:)>separability_lim)-0.1,'-*','LineWidth',2,'DisplayName',tags{2})
--- publications/IUS2020/Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM/process_CPWC_L7_4_probe_point_scatter.m
+++ publications/IUS2020/Rindal_et_al_Resolution_Measured_as_Separability_Compared_to_FWHM/process_CPWC_L7_4_probe_point_scatter.m
@@ -1,3 +1,4 @@
+% Process the images with adaptive beamformers under test
 clear all; close all;
 
 %% Channel Data
@@ -64,7 +65,6 @@ end
 %% Save UFF dataset
 % 
 % Finally, we can save the data into a UFF file.
-
 mid = midprocess.das();
 mid.channel_data=channel_data;
 mid.scan=sca;
@@ -79,7 +79,6 @@ mid.transmit_apodization.f_number=1.75;
 % Delay and sum on receive, then coherent compounding
 b_data_tx = mid.go();
 
-
 %% Add some noise to avoid "coherent" zero-signals
 b_data_tx.data = b_data_tx.data + randn(size(b_data_tx.data))*eps;
 
@@ -88,13 +87,8 @@ das=postprocess.coherent_compounding();
 das.input = b_data_tx;
 b_data_das = das.go();
 b_data_das.plot
-%das_img = b_data_das.get_image('none');  % Compensation weighting
-%das_img = db(abs(das_img./max(das_img(:))));                 % Normalize on max
-%f1 = figure(1);clf;
-%imagesc(b_data_das.scan.x_axis*1000,b_data_das.scan.z_axis*1000,das_img);
-%colormap gray;caxis([-120 0]);axis image;title('DAS');xlabel('x [mm]');ylabel('z [mm]');
 
-%%
+%% Coherence Factor
 cf = postprocess.coherence_factor();
 cf.dimension = dimension.receive;
 cf.receive_apodization = mid.receive_apodization;
@@ -102,13 +96,9 @@ cf.transmit_apodization = mid.transmit_apodization;
 cf.input = b_data_tx;
 b_data_cf = cf.go();
 b_data_cf.plot
-%cf_img = b_data_cf.get_image('none').*weights;
-%cf_img = db(abs(cf_img./max(cf_img(:))));                 % Normalize on max
-%f2 = figure(2);
-%imagesc(b_data_das.scan.x_axis*1000,b_data_das.scan.z_axis*1000,cf_img);
-%colormap gray;caxis([-60 0]);axis image;title('CF');xlabel('x [mm]');ylabel('z [mm]');
 
-%%
+
+%% Phase Coherence Factor
 pcf = postprocess.phase_coherence_factor();
 pcf.dimension = dimension.receive;
 pcf.receive_apodization = mid.receive_apodization;
@@ -116,29 +106,15 @@ pcf.transmit_apodization = mid.transmit_apodization;
 pcf.input = b_data_tx;
 b_data_pcf = pcf.go();
 b_data_pcf.plot
-%pcf_img = b_data_pcf.get_image('none').*weights;
-%pcf_img = db(abs(pcf_img./max(pcf_img(:))));
-%f3 = figure(3);clf;
-%imagesc(b_data_das.scan.x_axis*1000,b_data_das.scan.z_axis*1000,pcf_img);
-%colormap gray;caxis([-60 0]);axis image;title('PCF');xlabel('x [mm]');ylabel('z [mm]');
 
-%%
+%% Generalized Coherence Factor
 gcf=postprocess.generalized_coherence_factor();
-%gcf.dimension = dimension.receive;
-%gcf.transmit_apodization = mid.transmit_apodization;
-%gcf.receive_apodization = mid.receive_apodization;
 gcf.input = b_data_tx;
-%gcf.channel_data = channel_data;
 gcf.M0 = 2;
 b_data_gcf = gcf.go();
 b_data_gcf.plot()
-%gcf_img = b_data_gcf.get_image('none').*weights;
-%gcf_img = db(abs(gcf_img./max(gcf_img(:))));
-%f4 = figure(4);
-%imagesc(b_data_das.scan.x_axis*1000,b_data_das.scan.z_axis*1000,gcf_img);
-%colormap gray;caxis([-60 0]);axis image;title('GCF');xlabel('x [mm]');ylabel('z [mm]');
 
-%%
+%% Minimum Variance Capon Beamforming
 mv = postprocess.capon_minimum_variance();
 mv.dimension = dimension.receive;
 mv.transmit_apodization = mid.transmit_apodization;
@@ -152,11 +128,6 @@ mv.regCoef = 1/100;
 b_data_mv = mv.go();
 figure(5);clf;
 b_data_mv.plot()
-%mv_img = b_data_mv.get_image('none').*weights;
-%mv_img = db(abs(mv_img./max(mv_img(:))));
-%f5 = figure(7);clf;
-%imagesc(b_data_das.scan.x_axis*1000,b_data_das.scan.z_axis*1000,mv_img);
-%colormap gray;caxis([-60 0]);axis image;colorbar;title('MV');xlabel('x [mm]');ylabel('z [mm]');
 
 %% EIGENSPACE BASED MINIMUM VARIANCE
 ebmv=postprocess.eigenspace_based_minimum_variance();
@@ -185,7 +156,7 @@ figure(6);clf
 b_data_dmas.plot(6,['DMAS'])
 
 
-%%
+%% Save data to filename
 b_data_das.write([filename],'/b_data_das');
 b_data_cf.write([filename],'/b_data_cf');
 b_data_pcf.write([filename],'/b_data_pcf');
